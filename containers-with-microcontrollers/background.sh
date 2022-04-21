cebc40d59822dcf889436ed8ee9a94959592995e
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2022-04-21 12:16:01 +0200

    Update background.sh

--- containers-with-microcontrollers/background.sh
+++ containers-with-microcontrollers/background.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-echo "Hello from background script"
-echo "Install apt packages"
-sudo apt install llvm
-sudo apt install clang
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install git gcc-arm-none-eabi make gcc-multilib openocd gdb-multiarch doxygen wget unzip python3-serial
+sudo apt install llvm clang python3 python3-pip git gcc-arm-none-eabi make gcc-multilib openocd gdb-multiarch doxygen wget unzip python3-serial
 pip3 install pyelftools
 
-
-echo "Clone Git repository"
 git clone https://github.com/future-proof-iot/Femto-Container_tutorials.git
 cd Femto-Container_tutorials
 git submodule init
 git submodule update
+
+echo "done" >> /opt/.backgroundfinished
